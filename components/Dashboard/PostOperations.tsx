79074d3d783f62074dcddc8c4fc249da225dec5a
Nam <namdeveloper.ca@gmail.com>
2023-05-15 04:09:38 +0700

    alert dialog UI

--- components/Dashboard/PostOperations.tsx
+++ components/Dashboard/PostOperations.tsx
@@ -1,20 +1,45 @@
 'use client'
 
-import * as React from 'react'
-import Link from 'next/link'
 import { Post } from '@prisma/client'
+import Link from 'next/link'
+import { useState } from 'react'
 
+import { Icons } from '../icon'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Icons } from '../icon'
+} from '@ui/dropdown-menu'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@ui/alert-dialog'
+import { useRouter } from 'next/navigation'
 
 export function PostOperations({ post }: { post: Pick<Post, 'id' | 'title'> }) {
-  console.log('s')
+  const router = useRouter()
+
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false)
+  const [isDeleteLoading, setIsDeleteLoading] = useState(false)
+
+  async function onClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    // setIsDeleteLoading(true)
+    // const deleted = await deletePost(post.id)
+    // if (deleted) {
+    //   setIsDeleteLoading(false)
+    //   setShowDeleteAlert(false)
+    //   router.refresh()
+    // }
+  }
+
   return (
     <>
       <DropdownMenu>
@@ -29,11 +54,36 @@ export function PostOperations({ post }: { post: Pick<Post, 'id' | 'title'> }) {
             </Link>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem className='flex cursor-pointer items-center text-destructive focus:text-destructive'>
+          <DropdownMenuItem
+            onSelect={() => setShowDeleteAlert(true)}
+            className='flex cursor-pointer items-center text-destructive focus:text-destructive'
+          >
             Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure you want to delete this post?</AlertDialogTitle>
+            <AlertDialogDescription>This action cannot be undone.</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onClick(e)}
+              className='bg-red-600 focus:ring-red-600'
+            >
+              {isDeleteLoading ? (
+                <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+              ) : (
+                <Icons.trash className='mr-2 h-4 w-4' />
+              )}
+              <span>Delete</span>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   )
 }
