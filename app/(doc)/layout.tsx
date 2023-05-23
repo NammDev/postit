db9d88080e4c3c20a0cafa43b857bcc978ffc250
Nam <namdeveloper.ca@gmail.com>
2023-05-23 10:15:37 +0700

    Navbar Search, but not functional

--- app/(doc)/layout.tsx
+++ app/(doc)/layout.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 
 import { siteConfig } from '@/config/site'
-// import { DocsSearch } from '@/components/search'
-// import { DocsSidebarNav } from '@/components/sidebar-nav'
 import { SiteFooter } from '@/components/Nav/footer'
 import { Icons } from '@/components/icon'
 import { MainNav } from '@/components/Nav/MainNav'
 import { docsConfig } from '@/config/docs'
+import { DocsSearch } from '@/components/docs/search'
+import { DocsSidebarNav } from '@/components/docs/sidebar-nav'
 
 interface DocsLayoutProps {
   children: React.ReactNode
@@ -18,10 +18,12 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
       <header className='sticky top-0 z-40 w-full border-b bg-background'>
         <div className='container flex items-center h-16 space-x-4 sm:justify-between sm:space-x-0'>
           <MainNav items={docsConfig.mainNav}>
-            {/* <DocsSidebarNav items={docsConfig.sidebarNav} /> */}
+            <DocsSidebarNav items={docsConfig.sidebarNav} />
           </MainNav>
           <div className='flex items-center flex-1 space-x-4 sm:justify-end'>
-            <div className='flex-1 sm:grow-0'>{/* <DocsSearch /> */}</div>
+            <div className='flex-1 sm:grow-0'>
+              <DocsSearch />
+            </div>
             <nav className='flex space-x-4'>
               <Link href={siteConfig.links.github} target='_blank' rel='noreferrer'>
                 <Icons.gitHub className='h-7 w-7' />
